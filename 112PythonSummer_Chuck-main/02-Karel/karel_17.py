43461b83a03c8c3480687d93b6d7c248d04ec676
yuzhen67 <a111050233@mail.shu.edu.tw>
2024-08-18 01:47:50 +0800

    complete 17

--- 112PythonSummer_Chuck-main/02-Karel/karel_17.py
+++ 112PythonSummer_Chuck-main/02-Karel/karel_17.py
@@ -1,10 +1,32 @@
 from stanfordkarel import *
-import os
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def turn_around():
+    turn_left()
+    turn_left()
 
 def main():
     """ Karel code goes here! """
+    while True:
+        if right_is_clear():
+            turn_right()
+            move()
+            if not beepers_present():
+                put_beeper()
+            turn_around()
+            move()
+            turn_right()
+            move()
+        elif front_is_clear() and right_is_blocked():
+            move()
+        else:
+            break
     pass
 
 if __name__ == "__main__":
-    run_karel_program(os.path.join(os.getcwd(), 'worlds/fill_pothole'))
+    run_karel_program('./worlds/fill_pothole2.w')
     #run_karel_program(os.path.join(os.getcwd(), 'worlds/fill_pothole2'))
