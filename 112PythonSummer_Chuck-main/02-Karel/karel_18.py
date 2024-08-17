59fe3232621eb9d1cf7d60ea2fdf597d7cf1f7d2
yuzhen67 <a111050233@mail.shu.edu.tw>
2024-08-18 01:34:15 +0800

    complete 18

--- 112PythonSummer_Chuck-main/02-Karel/karel_18.py
+++ 112PythonSummer_Chuck-main/02-Karel/karel_18.py
@@ -1,10 +1,39 @@
 from stanfordkarel import *
-import os
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def climb_up():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    pass
+
+def climb_down():
+    move()
+    if right_is_blocked():
+        pass
+    else:
+        turn_right()
+        move()
+        turn_left()
+    pass
 
 def main():
     """ Karel code goes here! """
-    pass
-
+    while True:
+        if beepers_present():
+            pick_beeper()
+            break
+        elif front_is_blocked():
+            climb_up()
+        elif right_is_blocked() and front_is_clear():
+            climb_down()
+        else:
+            break
 if __name__ == "__main__":
-    run_karel_program(os.path.join(os.getcwd(), 'worlds/climb'))
+    run_karel_program('./worlds/climb.w')
     #run_karel_program(os.path.join(os.getcwd(), 'worlds/climb2'))
