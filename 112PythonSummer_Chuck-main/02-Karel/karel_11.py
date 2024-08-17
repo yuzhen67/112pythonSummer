73dc8232d1df9c61133b9a093888294c178e0551
yuzhen67 <a111050233@mail.shu.edu.tw>
2024-08-17 22:52:18 +0800

    complete 11

--- 112PythonSummer_Chuck-main/02-Karel/karel_11.py
+++ 112PythonSummer_Chuck-main/02-Karel/karel_11.py
@@ -1,7 +1,32 @@
 from stanfordkarel import *
 
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def turn_around():
+    turn_left()
+    turn_left()
+
 def main():
-    """ Karel code goes here! """
+    move()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    move()
+    pick_beeper()
+    turn_around()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_around()
+    
+    
     pass
 
 if __name__ == "__main__":
