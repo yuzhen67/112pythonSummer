b50ed2d998f0f0a737a7f74db0e6cbdcf2e68019
yuzhen67 <a111050233@mail.shu.edu.tw>
2024-08-17 23:07:52 +0800

    complete 13

--- 112PythonSummer_Chuck-main/02-Karel/karel_13.py
+++ 112PythonSummer_Chuck-main/02-Karel/karel_13.py
@@ -1,7 +1,35 @@
 from stanfordkarel import *
 
+def fix_the_column():
+    turn_left()
+    while front_is_clear():
+        if no_beepers_present():
+            put_beeper()
+        move()
+    if no_beepers_present():
+        put_beeper()
+
+def turn_around():
+    turn_left()
+    turn_left()
+
+def back_to_bottom():
+    turn_around()
+    while front_is_clear():
+        move()
+    turn_left()
+
+def next_column():
+    for i in range(4):
+        move()
+
 def main():
-    """ Karel code goes here! """
+    while front_is_clear():
+        fix_the_column()
+        back_to_bottom()
+        next_column()
+    fix_the_column()
+    back_to_bottom()
     pass
 
 if __name__ == "__main__":
